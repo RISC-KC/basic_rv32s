1698b6a0fc6a1ff11d9efb2fd7b0423497ada4b7
ChoiCube84 <wldnd84@naver.com>
2025-01-28 14:48:54 +0000

    [Feat] Add trap handling in PC Controller and testbench

--- RV32I/modules/PC_Controller.v
+++ RV32I/modules/PC_Controller.v
@@ -1,9 +1,11 @@
 module PCController (
-    input jump, 				// Signal indicating if we should jump
-	input branch_taken,			// Singal indicating if we should take the branch
+    input jump, 				// Signal indicating if PC should jump
+	input branch_taken,			// Signal indicating if PC should take the branch
+	input trapped,				// Signal indicating if trap has occurred
 	input [31:0] pc,			// Current pc value
 	input [31:0] jump_target,	// Target address for jumping
 	input [31:0] branch_target,	// Target address for taking branch
+	input [31:0] trap_target,	// Target address for trap
 	
 	output reg [31:0] next_pc	// Next pc value
 );
@@ -14,6 +16,9 @@ module PCController (
 		end
 		else if (branch_taken) begin
 			next_pc = branch_target;
+		end
+		else if (trapped) begin
+			next_pc = trap_target;
 		end
 		else begin
 			next_pc = pc + 4;
