883136b28d46568e64f8045e7ce5c5289f853974
ChoiCube84 <wldnd84@naver.com>
2025-02-07 13:07:49 +0000

    [Feat] Revise testbench for Exception Detector module

--- RV32I/testbenches/Exception_Detector_tb.v
+++ RV32I/testbenches/Exception_Detector_tb.v
@@ -6,6 +6,8 @@ module ExceptionDetector_tb;
 	reg [2:0] funct3;
 	reg [11:0] imm;
 	reg [31:0] jump_target;
+	reg branch;
+	reg [31:0] branch_target;
 	
     wire trapped;
 	wire [1:0] trap_status;
@@ -15,7 +17,9 @@ module ExceptionDetector_tb;
 		.funct3(funct3),
 		.imm_0(imm[0]),
 		.jump_target_lsbs(jump_target[1:0]),
-	
+		.branch(branch),
+		.branch_target_lsbs(branch_target[1:0]),
+		
     	.trapped(trapped),
 		.trap_status(trap_status)
     );
@@ -28,6 +32,8 @@ module ExceptionDetector_tb;
 		funct3 = 3'b0;
 		imm = 12'b0;
 		jump_target = 32'b0;
+		branch = 0;
+		branch_target = 32'b0;
 
         // Test 1: No exception
 		$display("\nNo exception: ");
@@ -52,6 +58,23 @@ module ExceptionDetector_tb;
         // Test 3: Address misaligned
 		$display("\nAddress misaligned: ");
 		
+		opcode = `OPCODE_BRANCH;
+		
+		branch = 0;
+		branch_target = 32'h000000F0; #1;
+        $display("opcode: %b, branch: %b, branch_target: %h, trapped: %b, trap_status: %b", opcode, branch, branch_target, trapped, trap_status);
+
+        branch_target = 32'h000000F1; #1;
+        $display("opcode: %b, branch: %b, branch_target: %h, trapped: %b, trap_status: %b", opcode, branch, branch_target, trapped, trap_status);
+		
+		branch = 1;
+		branch_target = 32'h000000F0; #1;
+        $display("opcode: %b, branch: %b, branch_target: %h, trapped: %b, trap_status: %b", opcode, branch, branch_target, trapped, trap_status);
+
+        branch_target = 32'h000000F1; #1;
+        $display("opcode: %b, branch: %b, branch_target: %h, trapped: %b, trap_status: %b\n", opcode, branch, branch_target, trapped, trap_status);
+		
+		branch = 0;
 		opcode = `OPCODE_JAL;
 		
 		jump_target = 32'h000000F0; #1;
