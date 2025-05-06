8c8d89b1ce0e16eae74a6aa0bafc4cd9488af1c4
ChoiCube84 <wldnd0804@gmail.com>
2025-05-06 10:36:38 +0900

    [Feat] Expand size of Data Cache

--- RV32I/modules/Data_Cache.v
+++ RV32I/modules/Data_Cache.v
@@ -1,35 +1,35 @@
 module DataCache (
     input clk,
     input reset,
-    input [9:0] address,
+    input [31:0] address,
     input read_enable,
     input write_enable,
     input [31:0] write_data,
     input [3:0] write_mask,
-    input [31:0] dm_data,
+    input [255:0] dm_data,
 
     output reg hit,
     output reg [31:0] read_data,
-    output reg [9:0] flush_address,
-    output reg [31:0] flush_data,
+    output reg [31:0] flush_address,
+    output reg [255:0] flush_data,
     output reg flush_done
 );
 
-    reg valid1 [0:7];
-    reg updated1 [0:7];
-    reg [4:0] tag1 [0:7];
-    reg [31:0] data1 [0:7];
+    reg valid1 [0:511];
+    reg updated1 [0:511];
+    reg [17:0] tag1 [0:511];
+    reg [255:0] data1 [0:511];
 
-    reg valid2 [0:7];
-    reg updated2 [0:7];
-    reg [4:0] tag2 [0:7];
-    reg [31:0] data2 [0:7];
+    reg valid2 [0:511];
+    reg updated2 [0:511];
+    reg [17:0] tag2 [0:511];
+    reg [255:0] data2 [0:511];
 
-    reg lru [0:7];
+    reg lru [0:511];
 
-    wire [1:0] offset = address[1:0]
-    wire [2:0] index = address[4:2];
-    wire [4:0] tag = address[9:5];
+    wire [4:0] offset = address[4:0];
+    wire [8:0] index = address[13:5];
+    wire [17:0] tag = address[31:14];
 
     wire hit1 = valid1[index] && (tag1[index] == tag);
     wire hit2 = valid2[index] && (tag2[index] == tag);
@@ -42,40 +42,40 @@ module DataCache (
     
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            for (i=0; i<32; i=i+1) begin
+            for (i=0; i<512; i=i+1) begin
                 valid1[i] <= 0;
                 updated1[i] <= 0;
-                tag1[i] <= 5'b0;
-                data1[i] <= 32'b0;
+                tag1[i] <= 18'b0;
+                data1[i] <= 256'b0;
 
                 valid2[i] <= 0;
                 updated2[i] <= 0;
-                tag2[i] <= 5'b0;
-                data2[i] <= 32'b0;
+                tag2[i] <= 18'b0;
+                data2[i] <= 256'b0;
                 
                 lru[i] <= 0;
             end
             
             read_data <= 32'b0;
-            flush_address <= 10'b0;
-            flush_data <= 10'b0;
+            flush_address <= 32'b0;
+            flush_data <= 256'b0;
             flush_done <= 1;
         end 
         else if (read_enable) begin
             if (hit1) begin
-                read_data <= data1[index];
+                read_data <= data1[index][32 * offset[4:2] +: 32];
                 lru[index] <= 1;
 
-                flush_address <= 10'b0;
-                flush_data <= 10'b0;
+                flush_address <= 32'b0;
+                flush_data <= 256'b0;
                 flush_done <= 1;
             end
             else if (hit2) begin
-                read_data <= data2[index];
+                read_data <= data2[index][32 * offset[4:2] +: 32];
                 lru[index] <= 0;
 
-                flush_address <= 10'b0;
-                flush_data <= 10'b0;
+                flush_address <= 32'b0;
+                flush_data <= 256'b0;
                 flush_done <= 1;
             end
             else begin
@@ -87,8 +87,8 @@ module DataCache (
                     data1[index] <= dm_data;
                     lru[index] <= 1;
 
-                    flush_address <= 10'b0;
-                    flush_data <= 32'b0;
+                    flush_address <= 32'b0;
+                    flush_data <= 256'b0;
                     flush_done <= 1;
                 end
                 else if (!valid2[index]) begin
@@ -97,13 +97,13 @@ module DataCache (
                     data2[index] <= dm_data;
                     lru[index] <= 0;
 
-                    flush_address <= 10'b0;
-                    flush_data <= 32'b0;
+                    flush_address <= 32'b0;
+                    flush_data <= 256'b0;
                     flush_done <= 1;
                 end
                 else if (lru[index] == 0) begin
                     if (updated1[index]) begin
-                        flush_address <= {tag1[index], index, 2'b0};
+                        flush_address <= {tag1[index], index, 5'b0};
                         flush_data <= data1[index];
                         flush_done <= 0;
 
@@ -114,14 +114,14 @@ module DataCache (
                         data1[index] <= dm_data;
                         lru[index] <= 1;
 
-                        flush_address <= 10'b0;
-                        flush_data <= 32'b0;
+                        flush_address <= 32'b0;
+                        flush_data <= 256'b0;
                         flush_done <= 1;
                     end
                 end
                 else begin
                     if (updated2[index]) begin
-                        flush_address <= {tag2[index], index, 2'b0};
+                        flush_address <= {tag2[index], index, 5'b0};
                         flush_data <= data2[index];
                         flush_done <= 0;
 
@@ -132,8 +132,8 @@ module DataCache (
                         data2[index] <= dm_data;
                         lru[index] <= 0;
 
-                        flush_address <= 10'b0;
-                        flush_data <= 32'b0;
+                        flush_address <= 32'b0;
+                        flush_data <= 256'b0;
                         flush_done <= 1;
                     end
                 end
@@ -146,29 +146,29 @@ module DataCache (
                 // Now we use for loop based assignment instead of using bitmasking
                 for (i=0; i<4; i=i+1) begin
                     if (write_mask[i]) begin
-                        data1[index][i*8 +: 8] <= write_data[i*8 +: 8];
+                        data1[index][32 * offset[4:2] + i*8 +: 8] <= write_data[i*8 +: 8];
                     end
                 end
 
                 updated1[index] <= 1;
                 lru[index] <= 1;
 
-                flush_address <= 10'b0;
-                flush_data <= 10'b0;
+                flush_address <= 32'b0;
+                flush_data <= 256'b0;
                 flush_done <= 1;
             end
             else if (hit2) begin
                 for (i=0; i<4; i=i+1) begin
                     if (write_mask[i]) begin
-                        data2[index][i*8 +: 8] <= write_data[i*8 +: 8];
+                        data2[index][32 * offset[4:2] + i*8 +: 8] <= write_data[i*8 +: 8];
                     end
                 end
 
                 updated2[index] <= 1;
                 lru[index] <= 0;
                 
-                flush_address <= 10'b0;
-                flush_data <= 10'b0;
+                flush_address <= 32'b0;
+                flush_data <= 256'b0;
                 flush_done <= 1;
             end
             else begin
@@ -178,18 +178,18 @@ module DataCache (
                     
                     for (i=0; i<4; i=i+1) begin
                         if (write_mask[i]) begin
-                            data1[index][i*8 +: 8] <= write_data[i*8 +: 8];
+                            data1[index][32 * offset[4:2] + i*8 +: 8] <= write_data[i*8 +: 8];
                         end
                         else begin
-                            data1[index][i*8 +: 8] <= dm_data[i*8 +: 8];
+                            data1[index][32 * offset[4:2] + i*8 +: 8] <= dm_data[32 * offset[4:2] + i*8 +: 8];
                         end
                     end
                     
                     updated1[index] <= 1;
                     lru[index] <= 1;
 
-                    flush_address <= 10'b0;
-                    flush_data <= 32'b0;
+                    flush_address <= 32'b0;
+                    flush_data <= 256'b0;
                     flush_done <= 1;
                 end
                 else if (!valid2[index]) begin
@@ -198,23 +198,23 @@ module DataCache (
                     
                     for (i=0; i<4; i=i+1) begin
                         if (write_mask[i]) begin
-                            data2[index][i*8 +: 8] <= write_data[i*8 +: 8];
+                            data2[index][32 * offset[4:2] + i*8 +: 8] <= write_data[i*8 +: 8];
                         end
                         else begin
-                            data2[index][i*8 +: 8] <= dm_data[i*8 +: 8];
+                            data2[index][32 * offset[4:2] + i*8 +: 8] <= dm_data[32 * offset[4:2] + i*8 +: 8];
                         end
                     end
 
                     updated2[index] <= 1;
                     lru[index] <= 0;
 
-                    flush_address <= 10'b0;
-                    flush_data <= 32'b0;
+                    flush_address <= 32'b0;
+                    flush_data <= 256'b0;
                     flush_done <= 1;
                 end
                 else if (lru[index] == 0) begin
                     if (updated1[index]) begin
-                        flush_address <= {tag1[index], index, 2'b0};
+                        flush_address <= {tag1[index], index, 5'b0};
                         flush_data <= data1[index];
                         flush_done <= 0;
 
@@ -225,24 +225,24 @@ module DataCache (
                         
                         for (i=0; i<4; i=i+1) begin
                             if (write_mask[i]) begin
-                                data1[index][i*8 +: 8] <= write_data[i*8 +: 8];
+                                data1[index][32 * offset[4:2] + i*8 +: 8] <= write_data[i*8 +: 8];
                             end
                             else begin
-                                data1[index][i*8 +: 8] <= dm_data[i*8 +: 8];
+                                data1[index][32 * offset[4:2] + i*8 +: 8] <= dm_data[32 * offset[4:2] + i*8 +: 8];
                             end
                         end
                         
                         updated1[index] <= 1;
                         lru[index] <= 1;
 
-                        flush_address <= 10'b0;
-                        flush_data <= 32'b0;
+                        flush_address <= 32'b0;
+                        flush_data <= 256'b0;
                         flush_done <= 1;
                     end
                 end
                 else begin
                     if (updated2[index]) begin
-                        flush_address <= {tag2[index], index, 2'b0};
+                        flush_address <= {tag2[index], index, 5'b0};
                         flush_data <= data2[index];
                         flush_done <= 0;
 
@@ -253,18 +253,18 @@ module DataCache (
                         
                         for (i=0; i<4; i=i+1) begin
                             if (write_mask[i]) begin
-                                data2[index][i*8 +: 8] <= write_data[i*8 +: 8];
+                                data2[index][32 * offset[4:2] + i*8 +: 8] <= write_data[i*8 +: 8];
                             end
                             else begin
-                                data2[index][i*8 +: 8] <= dm_data[i*8 +: 8];
+                                data2[index][32 * offset[4:2] + i*8 +: 8] <= dm_data[32 * offset[4:2] + i*8 +: 8];
                             end
                         end
 
                         updated2[index] <= 1;
                         lru[index] <= 0;
 
-                        flush_address <= 10'b0;
-                        flush_data <= 32'b0;
+                        flush_address <= 32'b0;
+                        flush_data <= 256'b0;
                         flush_done <= 1;
                     end
                 end
@@ -272,8 +272,8 @@ module DataCache (
         end
         else begin
             read_data <= 32'b0;
-            flush_address <= 10'b0;
-            flush_data <= 32'b0;
+            flush_address <= 32'b0;
+            flush_data <= 256'b0;
             flush_done <= 1;
         end
     end
--- RV32I/testbenches/Data_Cache_tb.v
+++ RV32I/testbenches/Data_Cache_tb.v
@@ -3,18 +3,17 @@
 module DataCache_tb;
     reg clk;
     reg reset;
-    reg [9:0] address;
+    reg [31:0] address;
     reg read_enable;
     reg write_enable;
     reg [31:0] write_data;
     reg [3:0] write_mask;
-    reg [31:0] dm_data;
+    reg [255:0] dm_data;
 
     wire hit;
     wire [31:0] read_data;
-    wire [9:0] flush_address;
-    wire [31:0] flush_data;
-    wire [3:0] flush_mask;
+    wire [31:0] flush_address;
+    wire [255:0] flush_data;
     wire flush_done;
 
     DataCache data_cache (
@@ -31,7 +30,6 @@ module DataCache_tb;
         .read_data(read_data),
         .flush_address(flush_address),
         .flush_data(flush_data),
-        .flush_mask(flush_mask),
         .flush_done(flush_done)
     );
 
@@ -55,10 +53,10 @@ module DataCache_tb;
         reset = 0; #10;
 
         // Scenario:
-        // 1. (Read) First line of 10101 index is updated with DEADBEEF. (tag: 00000) [Update -> Read]
-        // 2. (Read) Second line of 10101 index is updated with CAFEBABE. (tag: 00001) [Update -> Read]
-        // 3. (Write) 0x12 is written in most significant byte of First line of 10101 index. [Write]
-        // 4. (Read) First line of 10101 index is updated with 19721121 (tag: 00010) so tag 00000 should be flushed
+        // 1. (Read) First line of 101010101 index is updated with DEADBEEF. (tag: 000000000000000000) [Update -> Read]
+        // 2. (Read) Second line of 101010101 index is updated with CAFEBABE. (tag: 000000000000000001) [Update -> Read]
+        // 3. (Write) 0x12 is written in most significant byte of First line of 101010101 index. [Write]
+        // 4. (Read) First line of 101010101 index is updated with 19721121 (tag: 000000000000000010) so tag 000000000000000000 should be flushed
         //           because we updated from DEADBEEF to 12ADBEEF. [Flush -> Update -> Read]
         // 5. (Read) Second line of 10101 index is updated with 12ADBEEF (tag: 00000) and flush is not required. [Update -> Read]
         // 6. (Read) Read from first line. (tag: 00010) [Read]
@@ -67,8 +65,8 @@ module DataCache_tb;
         $display("Cache update: ");
         
         read_enable = 1;
-        address = 10'b00000_10101; 
-        dm_data = 32'hDEADBEEF; #10;
+        address = 32'b000000000000000000_101010101_00000; 
+        dm_data = 256'h00000000_00000000_00000000_00000000_00000000_00000000_00000000_DEADBEEF; #10;
 
         $display("Before: address: %b, hit: %b, read_data: %h", address, hit, read_data);
         #10; $display("After: address: %b, hit: %b, read_data: %h", address, hit, read_data);
@@ -76,8 +74,8 @@ module DataCache_tb;
         // Test 2: Update second way at same index
         $display("\nUpdate second way at same index: ");
 
-        address = 10'b00001_10101;
-        dm_data = 32'hCAFEBABE; #10;
+        address = 32'b000000000000000001_101010101_00000;
+        dm_data = 256'h00000000_00000000_00000000_00000000_00000000_00000000_00000000_CAFEBABE; #10;
         
         $display("Before: address: %b, hit: %b, read_data: %h", address, hit, read_data);
         #10; $display("After: address: %b, hit: %b, read_data: %h", address, hit, read_data);
@@ -87,14 +85,14 @@ module DataCache_tb;
 
         read_enable = 0;
         write_enable = 1;
-        address = 10'b00000_10101;
+        address = 32'b000000000000000000_101010101_00000;
         write_data = 32'h12345678; 
         write_mask = 4'b1000;
         #10;
         
         read_enable = 1;
         write_enable = 0;
-        address = 10'b00000_10101; 
+
         #10;
 
         $display("address: %b, hit: %b, read_data: %h", address, hit, read_data);
@@ -102,9 +100,16 @@ module DataCache_tb;
         // Test 4: LRU and flush
         $display("\nLRU and flush: ");
 
+        // read_enable = 1;
+        // write_enable = 0;
+        // address = 32'b000000000000000001_101010101_00000;
+        // #10;
+
+        // $display("address: %b, hit: %b, read_data: %h", address, hit, read_data);
+
         read_enable = 0;
         write_enable = 1;
-        address = 10'b00010_10101;
+        address = 32'b000000000000000010_101010101_00000;
         write_data = 32'h19721121; 
         write_mask = 4'b0011;
         #10;
@@ -129,14 +134,14 @@ module DataCache_tb;
 
         read_enable = 1;
         write_enable = 0;
-        address = 10'b00000_10101;
+        address = 32'b000000000000000000_101010101_00000;
         dm_data = 32'h12adbeef;
         #10;
 
         $display("\nBefore: address: %b, hit: %b, read_data: %h, flush_address: %b, flush_data: %h, flush_done: %b", address, hit, read_data, flush_address, flush_data, flush_done);
         #10; $display("After: address: %b, hit: %b, read_data: %h, flush_address: %b, flush_data: %h, flush_done: %b", address, hit, read_data, flush_address, flush_data, flush_done);
 
-        address = 10'b00010_10101; #10;
+        address = 32'b000000000000000010_101010101_00000; #10;
         
         $display("\naddress: %b, hit: %b, read_data: %h", address, hit, read_data);
 
