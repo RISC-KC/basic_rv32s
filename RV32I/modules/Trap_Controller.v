770616461bcdabbb7e40401939b38972b36c8cb6
T410N <aruru03102644@gmail.com>
2025-05-15 17:17:24 +0900

    [Feat] Modify TC logic by dividing existing sequential logics to combinational and sequential logic

--- RV32I/modules/Trap_Controller.v
+++ RV32I/modules/Trap_Controller.v
@@ -23,52 +23,36 @@ localparam  IDLE          = 2'b00,
             
 reg [1:0] trap_handle_state;
 
-always @(posedge clk or posedge reset) begin
-    //reset
-    if (reset) begin
-        trap_handle_state    <= IDLE;
-        debug_mode           <= 1'b0;
-        ic_clean             <= 1'b0;
-        trap_target          <= 32'b0;
-        csr_write_enable     <= 1'b0;
-        csr_trap_address     <= 12'b0;
-        csr_trap_write_data  <= 32'b0;
-        trap_done            <= 1'b1;
-    end else begin
-    // prevent latch synthesis (default value allocation)
-        debug_mode           <= 1'b0;
-        ic_clean             <= 1'b0;
-        csr_write_enable     <= 1'b0;
-        csr_trap_address     <= 12'b0;
-        csr_trap_write_data  <= 32'b0;
-        trap_target          <= 32'b0;
+always @(*) begin
+    debug_mode           <= 1'b0;
+    ic_clean             <= 1'b0;
+    csr_write_enable     <= 1'b0;
+    csr_trap_address     <= 12'b0;
+    csr_trap_write_data  <= 32'b0;
+    trap_target          <= 32'b0;
+    trap_done            <= 1'b1;
 
     case (trap_status)
         `TRAP_EBREAK: begin
-            trap_done           <= 1'b0;
-            debug_mode          <= 1'b1;
-            csr_write_enable    <= 1'b0;
-            case (trap_handle_state)
+        case (trap_handle_state)
             //Write mepc mode
-                IDLE: begin
-                    trap_done           <= 1'b0;
-                    csr_write_enable    <= 1'b1;
-                    csr_trap_address    <= 12'h341;     //mepc
-                    csr_trap_write_data <= pc;
-                    trap_handle_state   <= WRITE_MEPC;
+            IDLE: begin
+                trap_done           <= 1'b0;
+                csr_write_enable    <= 1'b1;
+                csr_trap_address    <= 12'h341;     //mepc
+                csr_trap_write_data <= pc;
             end
             //Write mcause mode
-                WRITE_MEPC: begin
-                    trap_done           <= 1'b0;
-                    csr_write_enable    <= 1'b1;
-                    csr_trap_address    <= 12'h342; //mcause
-                    csr_trap_write_data <= 32'd3; //mcause value 3 = Breakpoint exception code
-                    trap_handle_state   <= WRITE_MCAUSE;
+            WRITE_MEPC: begin
+                trap_done           <= 1'b0;
+                csr_write_enable    <= 1'b1;
+                csr_trap_address    <= 12'h342; //mcause
+                csr_trap_write_data <= 32'd3; //mcause value 3 = Breakpoint exception code
             end
             //Pre-Trap Handling done state
-                WRITE_MCAUSE: begin
-                    csr_write_enable     <= 1'b0;
-                    trap_done <= 1'b1;
+            WRITE_MCAUSE: begin
+                csr_write_enable     <= 1'b0;
+                trap_done <= 1'b1;
             end
             endcase
         end
@@ -77,32 +61,29 @@ always @(posedge clk or posedge reset) begin
             csr_write_enable    <= 1'b0;
             case (trap_handle_state)
             //Write mepc mode
-                IDLE: begin
-                    trap_done           <= 1'b0;
-                    csr_write_enable    <= 1'b1;
-                    csr_trap_address    <= 12'h341;     //mepc
-                    csr_trap_write_data <= pc;
-                    trap_handle_state   <= WRITE_MEPC;
+            IDLE: begin
+                trap_done           <= 1'b0;
+                csr_write_enable    <= 1'b1;
+                csr_trap_address    <= 12'h341;     //mepc
+                csr_trap_write_data <= pc;
             end
             //Write mcause mode
-                WRITE_MEPC: begin
-                    trap_done           <= 1'b0;
-                    csr_write_enable    <= 1'b1;
-                    csr_trap_address    <= 12'h342; //mcause
-                    csr_trap_write_data <= 32'd11; //mcause value 11 = Environment call from M-mode exception code
-                    trap_handle_state   <= WRITE_MCAUSE;
+            WRITE_MEPC: begin
+                trap_done           <= 1'b0;
+                csr_write_enable    <= 1'b1;
+                csr_trap_address    <= 12'h342; //mcause
+                csr_trap_write_data <= 32'd11; //mcause value 11 = Environment call from M-mode exception code
             end
             //Read mtvec mode
-                WRITE_MCAUSE: begin
-                    trap_done         <= 1'b0;
-                    csr_write_enable  <= 1'b0;
-                    csr_trap_address  <= 12'h305; //mtvec
-                    trap_target       <= csr_read_data;
-                    trap_handle_state <= READ_MTVEC;
+            WRITE_MCAUSE: begin
+                trap_done         <= 1'b0;
+                csr_write_enable  <= 1'b0;
+                csr_trap_address  <= 12'h305; //mtvec
+                trap_target       <= csr_read_data;
             end
             //Pre-Trap Handling done state
-                READ_MTVEC: begin
-                    trap_done <= 1'b1;
+            READ_MTVEC: begin
+                trap_done <= 1'b1;
             end
             endcase
         end
@@ -111,33 +92,30 @@ always @(posedge clk or posedge reset) begin
             csr_write_enable    <= 1'b0;
             case (trap_handle_state)
             //Write mepc mode
-                IDLE: begin
-                    trap_done           <= 1'b0;
-                    csr_write_enable    <= 1'b1;
-                    trap_done           <= 1'b0;
-                    csr_trap_address    <= 12'h341;     //mepc
-                    csr_trap_write_data <= pc;
-                    trap_handle_state   <= WRITE_MEPC;
+            IDLE: begin
+                trap_done           <= 1'b0;
+                csr_write_enable    <= 1'b1;
+                trap_done           <= 1'b0;
+                csr_trap_address    <= 12'h341;     //mepc
+                csr_trap_write_data <= pc;
             end
             //Write mcause mode
-                WRITE_MEPC: begin
-                    trap_done           <= 1'b0;
-                    csr_write_enable    <= 1'b1;
-                    csr_trap_address    <= 12'h342; //mcause
-                    csr_trap_write_data <= 32'd0; //mcause value 0 = Instruction address misaligned exception code
-                    trap_handle_state   <= WRITE_MCAUSE;
+            WRITE_MEPC: begin
+                trap_done           <= 1'b0;
+                csr_write_enable    <= 1'b1;
+                csr_trap_address    <= 12'h342; //mcause
+                csr_trap_write_data <= 32'd0; //mcause value 0 = Instruction address misaligned exception code
             end
             //Read mtvec mode
-                WRITE_MCAUSE: begin
-                    trap_done         <= 1'b0;
-                    csr_write_enable  <= 1'b0;
-                    csr_trap_address  <= 12'h305; //mtvec
-                    trap_target       <= csr_read_data;
-                    trap_handle_state <= READ_MTVEC;
+            WRITE_MCAUSE: begin
+                trap_done         <= 1'b0;
+                csr_write_enable  <= 1'b0;
+                csr_trap_address  <= 12'h305; //mtvec
+                trap_target       <= csr_read_data;
                 end
             //Pre-Trap Handling done state
-                READ_MTVEC: begin
-                    trap_done <= 1'b1;
+            READ_MTVEC: begin
+                trap_done <= 1'b1;
             end
             endcase
         end
@@ -153,7 +131,6 @@ always @(posedge clk or posedge reset) begin
             csr_trap_address   <= 12'h341; //mepc
             trap_target        <= csr_read_data + 4;
             debug_mode         <= 1'b0;
-            trap_handle_state  <= IDLE;
             trap_done          <= 1'b1;
         end
 
@@ -166,6 +143,37 @@ always @(posedge clk or posedge reset) begin
         end
     endcase
 end
+
+always @(posedge clk or posedge reset) begin
+    //reset
+    if (reset) begin
+        trap_handle_state <= IDLE;
+    end else begin
+        case (trap_handle_state)
+            IDLE: begin
+                if (trap_status != `TRAP_NONE && trap_status != `TRAP_FENCEI && trap_status != `TRAP_MRET) begin
+                trap_handle_state <= WRITE_MEPC;
+            end 
+            end
+            WRITE_MEPC: begin
+                if (trap_status != `TRAP_NONE && trap_status != `TRAP_FENCEI && trap_status != `TRAP_MRET) begin
+                trap_handle_state <= WRITE_MCAUSE;
+            end 
+            end 
+            WRITE_MCAUSE: begin
+                if (trap_status == `TRAP_ECALL || trap_status == `TRAP_MISALIGNED) begin
+                trap_handle_state <= READ_MTVEC;
+            end else begin
+                trap_handle_state <= IDLE;
+            end
+            end 
+            READ_MTVEC: begin
+                trap_handle_state <= IDLE;
+            end 
+            
+            default: trap_handle_state <= IDLE;
+        endcase
+    end
 end
 
 endmodule
