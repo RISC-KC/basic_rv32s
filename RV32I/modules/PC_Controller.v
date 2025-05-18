f922613021aef61f6f114b508b5e758b58918a59
T410N <aruru03102644@gmail.com>
2025-05-18 15:02:25 +0900

    [Feat] implement PCC micro opcode for next_pc race issue and its testbench

--- RV32I/modules/PC_Controller.v
+++ RV32I/modules/PC_Controller.v
@@ -1,32 +1,43 @@
+`include "modules/headers/pcc_op.vh"
+
 module PCController (
-    input jump, 				// signal indicating if PC should jump
-	input branch_taken,			// signal indicating if PC should take the branch
-	input trapped,				// signal indicating if trap has occurred
+    input [2:0]  pcc_op,		// program counter operation code from Control Unit
 	input [31:0] pc,			// current pc value
 	input [31:0] jump_target,	// target address for jump
-	input [31:0] imm,			// immediate value from Immediate Generator
+	input [31:0] branch_target,	// target address for branch
 	input [31:0] trap_target,	// target address for trap
-	input pc_stall,				// signal indicating if pc update should be paused
 
 	output reg [31:0] next_pc	// next pc value
 );
 
     always @(*) begin
-		if (pc_stall) begin
-			next_pc = pc;
-		end
-		else if (jump && pc_stall == 0 && trapped == 0 && branch_taken == 0) begin
-			next_pc = jump_target;
-		end
-		else if (branch_taken && jump == 0 && pc_stall == 0 && trapped == 0) begin
-			next_pc = pc + imm;
-		end
-		else if (trapped && jump == 0 && pc_stall == 0 && branch_taken == 0) begin
-			next_pc = trap_target;
-		end
-		else begin
-			next_pc = pc + 4;
-		end
+		case (pcc_op)
+			`PCC_NONE: begin
+				next_pc = pc + 4;
+			end
+			`PCC_STALL: begin
+				next_pc = pc;
+			end
+			`PCC_JUMP: begin
+				if (jump_target[1:0] != 2'b0) begin
+					next_pc = pc;
+				end
+				else begin
+					next_pc = jump_target;
+				end
+			end
+			`PCC_BTAKEN: begin
+				if (branch_target[1:0] != 2'b0) begin
+					next_pc = pc;
+				end
+				else begin
+					next_pc = branch_target;
+				end
+			end
+			`PCC_TRAPPED: begin
+				next_pc = trap_target;
+			end
+		endcase
 	end
 
 endmodule
