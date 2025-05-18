6d3af9f60bf54c46a04d6a544c3650b8da0781cf
T410N <aruru03102644@gmail.com>
2025-05-18 20:44:38 +0900

    [Feat] Modified signal name by revising instr signal to instruction signal and its testbench

--- RV32I/modules/Instruction_Decoder.v
+++ RV32I/modules/Instruction_Decoder.v
@@ -1,7 +1,7 @@
 `include "modules/headers/opcode.vh"
 
 module InstructionDecoder (
-    input [31:0] instr,
+	input [31:0] instruction,
     
     output reg [6:0] opcode,
 	output reg [2:0] funct3,
@@ -11,14 +11,12 @@ module InstructionDecoder (
 	output reg [4:0] rd,
 	output reg [19:0] raw_imm
 );
-
     always @(*) begin
-		opcode = instr[6:0];
-		
+		opcode = instruction[6:0];
         case (opcode)
 			`OPCODE_LUI, `OPCODE_AUIPC: begin // U-type
-                rd = instr[11:7];
-				raw_imm = instr[31:12];
+                rd = instruction[11:7];
+				raw_imm = instruction[31:12];
 				
 				funct3 = 3'b000;
 				rs1 = 5'b00000;
@@ -27,8 +25,8 @@ module InstructionDecoder (
             end
 			
 			`OPCODE_JAL: begin // J-type
-				rd = instr[11:7];
-				raw_imm = {instr[31], instr[19:12], instr[20], instr[30:21]};
+				rd = instruction[11:7];
+				raw_imm = {instruction[31], instruction[19:12], instruction[20], instruction[30:21]};
 				
 				funct3 = 3'b000;
 				rs1 = 5'b00000;
@@ -37,41 +35,41 @@ module InstructionDecoder (
 			end
 			
 			`OPCODE_JALR, `OPCODE_LOAD, `OPCODE_ITYPE, `OPCODE_FENCE, `OPCODE_ENVIRONMENT: begin // I-type
-				rd = instr[11:7];
-				funct3 = instr[14:12];
-				rs1 = instr[19:15];
-				raw_imm = {8'b0, instr[31:20]};
+				rd = instruction[11:7];
+				funct3 = instruction[14:12];
+				rs1 = instruction[19:15];
+				raw_imm = {8'b0, instruction[31:20]};
 				
 				rs2 = 5'b00000;
 				funct7 = 7'b0000000;
 			end
 			
 			`OPCODE_BRANCH: begin // B-type
-				funct3 = instr[14:12];
-				rs1 = instr[19:15];
-				rs2 = instr[24:20];
-				raw_imm = {instr[31], instr[7], instr[30:25], instr[11:8]};
+				funct3 = instruction[14:12];
+				rs1 = instruction[19:15];
+				rs2 = instruction[24:20];
+				raw_imm = {instruction[31], instruction[7], instruction[30:25], instruction[11:8]};
 				
 				rd = 5'b00000;
 				funct7 = 7'b0000000;
 			end
 			
 			`OPCODE_STORE: begin // S-type
-				funct3 = instr[14:12];
-				rs1 = instr[19:15];
-				rs2 = instr[24:20];
-				raw_imm = {8'b0, instr[31:25], instr[11:7]};
+				funct3 = instruction[14:12];
+				rs1 = instruction[19:15];
+				rs2 = instruction[24:20];
+				raw_imm = {8'b0, instruction[31:25], instruction[11:7]};
 				
 				rd = 5'b00000;
 				funct7 = 7'b0000000;
 			end
 			
 			`OPCODE_RTYPE: begin // R type
-				rd = instr[11:7];
-				funct3 = instr[14:12];
-				rs1 = instr[19:15];
-				rs2 = instr[24:20];
-				funct7 = instr[31:25];
+				rd = instruction[11:7];
+				funct3 = instruction[14:12];
+				rs1 = instruction[19:15];
+				rs2 = instruction[24:20];
+				funct7 = instruction[31:25];
 				
 				raw_imm = 32'b0;
 			end
