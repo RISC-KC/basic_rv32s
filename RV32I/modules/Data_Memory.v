7422035d6ec3500da3a76fd8585d3bd5f683d233
T410N <aruru03102644@gmail.com>
2025-05-21 18:07:17 +0900

    [Feat] Separate non cahce based data memory and cahce based data memory for RV32I46F

--- RV32I/modules/Data_Memory.v
+++ RV32I/modules/Data_Memory.v
@@ -1,39 +1,32 @@
 module DataMemory (
     input clk,                      // clock signal
     input write_enable,             // enabling signal for writing Data Memory
-    input [31:0] address,            // Take address of memory to read/write value
+    input [9:0] address,            // Take address of memory to read/write value
     input [31:0] write_data,        // data to write to Data Memory
-    input [3:0] write_mask,         // bitmask for writing data
+    input [3:0] write_mask,         // bitmask for writing data (Should receive 4'b1111 in RV32I47F and 50F top module)
 
-    output reg [255:0] read_data    // data block (8 words) read from Data Memory
+    output reg [31:0] read_data    // data read from Data Memory
 );
 
-    parameter VALID_ADDRESS_WIDTH = 18; // 27 for 512MB, 18 for 1MB
-
-    reg [31:0] memory [0:(2<<VALID_ADDRESS_WIDTH)-1]; // 134217728(2^27) words (512MB)
+    reg [31:0] memory [0:1023];     // 1024 words (4KB)
+    
+    // 32 bit extended mask from 4 bit write_mask
+    wire [31:0] extended_mask = {{8{write_mask[3]}}, {8{write_mask[2]}}, {8{write_mask[1]}}, {8{write_mask[0]}}};
 
     integer i;
-
     initial begin
-        for (i=0; i<VALID_ADDRESS_WIDTH; i=i+1) begin
-            memory[i] <= 32'b0;
+        for (i=0; i<1024; i=i+1) begin
+            memory[i] = 32'b0;
         end
     end
-    
-    always @(posedge clk) begin
-        read_data <= {
-            memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b111}], memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b110}], 
-            memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b101}], memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b100}],
-            memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b011}], memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b010}], 
-            memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b001}], memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b000}]
-        };
 
+    always @(*) begin
+        read_data = memory[address];
+    end
+
+    always @(posedge clk) begin
         if (write_enable) begin
-            for (i=0; i<4; i=i+1) begin
-                if (write_mask[i]) begin
-                    memory[address[VALID_ADDRESS_WIDTH+1:2]][i*8 +: 8] <= write_data[i*8 +: 8];
-                end
-            end
+            memory[address] <= ((memory[address] & ~extended_mask) | (write_data & extended_mask));
         end
     end
 
---
+++ RV32I/modules/Data_Memory_For_Cache.v
@@ -0,0 +1,40 @@
+module DataMemoryForCache (
+    input clk,                      // clock signal
+    input write_enable,             // enabling signal for writing Data Memory
+    input [31:0] address,            // Take address of memory to read/write value
+    input [31:0] write_data,        // data to write to Data Memory
+    input [3:0] write_mask,         // bitmask for writing data
+
+    output reg [255:0] read_data    // data block (8 words) read from Data Memory
+);
+
+    parameter VALID_ADDRESS_WIDTH = 18; // 27 for 512MB, 18 for 1MB
+
+    reg [31:0] memory [0:(2<<VALID_ADDRESS_WIDTH)-1]; // 134217728(2^27) words (512MB)
+
+    integer i;
+
+    initial begin
+        for (i=0; i<VALID_ADDRESS_WIDTH; i=i+1) begin
+            memory[i] <= 32'b0;
+        end
+    end
+    
+    always @(posedge clk) begin
+        read_data <= {
+            memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b111}], memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b110}], 
+            memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b101}], memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b100}],
+            memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b011}], memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b010}], 
+            memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b001}], memory[{address[VALID_ADDRESS_WIDTH+1:5], 3'b000}]
+        };
+
+        if (write_enable) begin
+            for (i=0; i<4; i=i+1) begin
+                if (write_mask[i]) begin
+                    memory[address[VALID_ADDRESS_WIDTH+1:2]][i*8 +: 8] <= write_data[i*8 +: 8];
+                end
+            end
+        end
+    end
+
+endmodule
