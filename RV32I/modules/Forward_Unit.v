2ffd59d2f1d392d9bda4f8fea8c4263461f170d9
T410N <aruru03102644@gmail.com>
2025-05-25 13:17:52 +0900

    [Feat] Implement Forward Unit module and its testbench

--- RV32I/modules/Forward_Unit.v
+++ RV32I/modules/Forward_Unit.v
@@ -1,28 +1,38 @@
-module ForwardUnit (
-    input wire hazard_op                    // signal indicating data hazard has been occured
-    input wire [31:0] MEM_imm;              // from EX/MEM Register for LUI
-    input wire [31:0] MEM_alu_result;       
-    input wire [31:0] MEM_csr_read_data;
-    input wire [31:0] MEM_read_data;        // from Data Memory
-    input wire [31:0] MEM_pc_plus_4;        // from EX/MEM Register
-    input wire [6:0] MEM_opcode;            // from EX/MEM Register
-    
-    output reg [31:0] alu_forward_source_data_a;    // Forwarded source A data signal
-    output reg [31:0] alu_forward_source_data_b;    // Forwarded source B data signal
-    output reg [1:0] alu_forward_source_select_a; // ALU source A selection between normal source and forwarded source
-    output reg [1:0] alu_forward_source_select_b; // ALU source B selection between normal source and forwarded source
+`include "modules/headers/opcode.vh"
 
-    localparam OPCODE_LOAD = ;
+module ForwardUnit #(
+    parameter XLEN = 32
+)(
+    input wire [1:0] hazard_op,                    // signal indicating data hazard has been occured
+    input wire [XLEN-1:0] MEM_imm,              // from EX/MEM Register for LUI
+    input wire [XLEN-1:0] MEM_alu_result,       
+    input wire [XLEN-1:0] MEM_csr_read_data,
+    input wire [XLEN-1:0] MEM_read_data,        // from Data Memory
+    input wire [XLEN-1:0] MEM_pc_plus_4,        // from EX/MEM Register
+    input wire [6:0] MEM_opcode,            // from EX/MEM Register
+    
+    output wire [XLEN-1:0] alu_forward_source_data_a,    // Forwarded source A data signal
+    output wire [XLEN-1:0] alu_forward_source_data_b,    // Forwarded source B data signal
+    output wire [1:0] alu_forward_source_select_a, // ALU source A selection between normal source and forwarded source
+    output wire [1:0] alu_forward_source_select_b // ALU source B selection between normal source and forwarded source
+);
+    reg [31:0] forward_data_value;
+
+    assign alu_forward_source_select_a = hazard_op[0] ? 2'b10 : 2'b01;
+    assign alu_forward_source_select_b = hazard_op[1] ? 2'b10 : 2'b01;
+
+    assign alu_forward_source_data_a = hazard_op[0] ? forward_data_value : {XLEN{1'b0}};
+    assign alu_forward_source_data_b = hazard_op[1] ? forward_data_value : {XLEN{1'b0}};
 
     always @(*) begin
-        if (hazard_op == 1'b1) begin
-            case (param)
-                : 
-                default: 
-            endcase
-        end
-        
+        case (MEM_opcode)
+            `OPCODE_LOAD : forward_data_value = MEM_read_data;
+            `OPCODE_ENVIRONMENT : forward_data_value = MEM_csr_read_data;
+            `OPCODE_LUI : forward_data_value = MEM_imm;
+            `OPCODE_JAL :  forward_data_value = MEM_pc_plus_4;
+            `OPCODE_JALR : forward_data_value = MEM_pc_plus_4;
+            default: forward_data_value = MEM_alu_result;
+        endcase
     end
-);
     
 endmodule
---
+++ RV32I/testbenches/Forward_Unit_tb.v
@@ -0,0 +1,84 @@
+`timescale 1ns/1ps
+`include "modules/headers/opcode.vh"
+
+module ForwardUnit_tb;
+localparam XLEN = 32;
+
+reg [1:0]  hazard_op;
+reg [XLEN-1:0] MEM_imm;
+reg [XLEN-1:0] MEM_alu_result;
+reg [XLEN-1:0] MEM_csr_read_data;
+reg [XLEN-1:0] MEM_read_data;
+reg [XLEN-1:0] MEM_pc_plus_4;
+reg [6:0] MEM_opcode;
+
+wire [XLEN-1:0] alu_forward_source_data_a;
+wire [XLEN-1:0] alu_forward_source_data_b;
+wire [1:0] alu_forward_source_select_a;
+wire [1:0] alu_forward_source_select_b;
+
+ForwardUnit forward_unit (
+    .hazard_op                 (hazard_op),
+    .MEM_imm                   (MEM_imm),
+    .MEM_alu_result            (MEM_alu_result),
+    .MEM_csr_read_data         (MEM_csr_read_data),
+    .MEM_read_data             (MEM_read_data),
+    .MEM_pc_plus_4             (MEM_pc_plus_4),
+    .MEM_opcode                (MEM_opcode),
+    .alu_forward_source_data_a (alu_forward_source_data_a),
+    .alu_forward_source_data_b (alu_forward_source_data_b),
+    .alu_forward_source_select_a(alu_forward_source_select_a),
+    .alu_forward_source_select_b(alu_forward_source_select_b)
+);
+
+initial begin
+    $dumpfile("testbenches/results/waveforms/ForwardUnit_tb.vcd");
+    $dumpvars(0, ForwardUnit_tb);
+
+    $display("==================== Forward Unit Test START ====================");
+    // Initialize signals
+    hazard_op = 2'b00;
+    MEM_imm   = 32'hAAAA_0000;
+    MEM_alu_result    = 32'hDEAD_BEEF;
+    MEM_csr_read_data = 32'hFACE_CAFE;
+    MEM_read_data     = 32'h1111_2222;
+    MEM_pc_plus_4     = 32'h0040_1004;
+
+    // Test 0 : no hazard (sel = 01, data = 0)
+    MEM_opcode = `OPCODE_RTYPE; // ALU
+    #1;
+    $display("Test 0 : no hazard (sel = 01, data = 0)");
+    $display("selA = %b selB = %b fwdA = %h fwdB = %h\n", alu_forward_source_select_a, alu_forward_source_select_b, alu_forward_source_data_a, alu_forward_source_data_b);
+
+    // Test 1 : rs1 hazard only (LOAD)
+    hazard_op = 2'b01;
+    MEM_opcode = `OPCODE_LOAD;
+    #1;
+    $display("Test 1 : rs1 hazard only (LOAD)");
+    $display("selA = %b selB = %b fwdA = %h fwdB = %h\n", alu_forward_source_select_a, alu_forward_source_select_b, alu_forward_source_data_a, alu_forward_source_data_b);
+    
+    // Test 2 : rs2 hazard only (JALR)
+    hazard_op = 2'b10;
+    MEM_opcode = `OPCODE_JALR;
+    #1;
+    $display("Test 2 : rs2 hazard only (JALR)");
+    $display("selA = %b selB = %b fwdA = %h fwdB = %h\n", alu_forward_source_select_a, alu_forward_source_select_b, alu_forward_source_data_a, alu_forward_source_data_b);
+
+    // Test 0 (again) : no hazard (sel=01, data=0)
+    MEM_opcode = `OPCODE_RTYPE; // ALU
+    hazard_op = 2'b00;
+    #1;
+    $display("Test 0 : no hazard (sel = 01, data = 0)");
+    $display("selA = %b selB = %b fwdA = %h fwdB = %h\n", alu_forward_source_select_a, alu_forward_source_select_b, alu_forward_source_data_a, alu_forward_source_data_b);
+
+    // Test 3 : both hazards (CSR) ------------
+    hazard_op = 2'b11;
+    MEM_opcode = `OPCODE_ENVIRONMENT;
+    #1;
+    $display("Test 3 : both hazards (CSR)");
+    $display("selA = %b selB = %b fwdA = %h fwdB = %h\n", alu_forward_source_select_a, alu_forward_source_select_b, alu_forward_source_data_a, alu_forward_source_data_b);
+
+    $display("\n====================  Forward Unit Test END  ====================");
+    $finish;
+end
+endmodule
