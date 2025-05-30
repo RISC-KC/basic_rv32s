3ed78ea522bb1294be663b5f720d6b0719eb4506
T410N <aruru03102644@gmail.com>
2025-05-30 12:05:26 +0900

    [Feat] Add instruction and pc signals for debugging each phase of pipeline registers

--- RV32I/modules/MEM_WB_Register.v
+++ RV32I/modules/MEM_WB_Register.v
@@ -7,7 +7,9 @@ module MEM_WB_Register #(
     input wire flush,
 
     // signals from EX/MEM register
+    input wire [XLEN-1:0] MEM_pc,
     input wire [XLEN-1:0] MEM_pc_plus_4,
+    input wire [XLEN-1:0] MEM_instruction,
 
     input wire [2:0] MEM_register_file_write_data_select,
     input wire [XLEN-1:0] MEM_imm,
@@ -21,7 +23,9 @@ module MEM_WB_Register #(
     input wire [XLEN-1:0] MEM_byte_enable_logic_register_file_write_data,
 
     // signals to MEM register
+    output reg [XLEN-1:0] WB_pc,
     output reg [XLEN-1:0] WB_pc_plus_4,
+    output reg [XLEN-1:0] WB_instruction,
 
     output reg [2:0] WB_register_file_write_data_select,
     output reg [XLEN-1:0] WB_imm,
@@ -36,7 +40,9 @@ module MEM_WB_Register #(
 
 always @(posedge clk or posedge reset) begin
     if (reset || flush) begin
+        WB_pc <= 32'b0;
         WB_pc_plus_4 <= {XLEN{1'b0}};
+        WB_instruction <= 32'h0000_0013;
 
         WB_register_file_write_data_select <= 3'b0;
         WB_imm <= {XLEN{1'b0}};
@@ -48,7 +54,8 @@ always @(posedge clk or posedge reset) begin
         
         WB_byte_enable_logic_register_file_write_data <= {XLEN{1'b0}};
     end else begin
-        WB_pc_plus_4 <= MEM_pc_plus_4;
+        WB_pc <= MEM_pc;
+        WB_instruction <= MEM_instruction;
 
         WB_register_file_write_data_select <= MEM_register_file_write_data_select;
         WB_imm <= MEM_imm;
