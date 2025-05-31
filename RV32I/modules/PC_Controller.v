681a27405567a9733ddfebc52725b178182bc228
unknown <aruru03102644@gmail.com>
2025-05-31 18:18:44 +0900

    [Feat] Revise PC Controller by dualizing the branch_target signal and priortizing conditions for the next_pc decision

--- RV32I/modules/PC_Controller.v
+++ RV32I/modules/PC_Controller.v
@@ -1,10 +1,12 @@
 module PCController (
     input jump, 				// signal indicating if PC should jump
 	input branch_estimation,	// signal indicating if PC should take the branch
+	input branch_prediction_miss,	
 	input trapped,				// signal indicating if trap has occurred
 	input [31:0] pc,			// current pc value
 	input [31:0] jump_target,	// target address for jump
 	input [31:0] branch_target, // target address for branch from branch predictor
+	input [31:0] branch_target_actual, // actual branch target address when mispredicted
 	input [31:0] trap_target,	// target address for trap
 	input pc_stall,				// signal indicating if pc update should be paused
 
@@ -16,10 +18,13 @@ module PCController (
 			if (trapped) begin
 				next_pc = trap_target;
 			end
-			else if (branch_estimation) begin
+			else if (trapped == 1'b0 && branch_prediction_miss) begin
+				next_pc = branch_target_actual;
+			end
+			else if (trapped == 1'b0 && branch_estimation) begin
 				next_pc = branch_target;
 			end
-			else if (jump) begin
+			else if (trapped == 1'b0 && jump) begin
 				next_pc = jump_target;
 			end
 			else begin
