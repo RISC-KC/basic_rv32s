8b4b04412ba0303d9f84b74ef6f8a4a206b091bb
T410N <aruru03102644@gmail.com>
2025-06-10 20:56:40 +0900

    [Feat] Revise RV32I46F_5SP by implementing Synchronous CSR File in top module design and its testebench

--- RV32I/testbenches/RV32I46F_5SP_tb.v
+++ RV32I/testbenches/RV32I46F_5SP_tb.v
@@ -13,7 +13,7 @@ module RV32I46F5SP_tb;
     always #5 clk = ~clk;
 
     initial begin
-        $dumpfile("testbenches/results/waveforms/RV32I46F5SP_opt_tb_result2.vcd");
+        $dumpfile("testbenches/results/waveforms/RV32I46F5SP_tb_sync_csr.vcd");
         $dumpvars(0, rv32i46f_5sp);
 
         $display("==================== RV32I46F_5SP Test START ====================");
@@ -25,7 +25,7 @@ module RV32I46F5SP_tb;
 
         reset = 0;
 
-        #1315;
+        #2315;
 
         $display("\n====================  RV32I46F_5SP Test END  ====================");
         $stop;
